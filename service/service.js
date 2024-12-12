82769d1e2184126f4707f436cc905debb60532d7
Morpheus133 <morpheus1333@gmail.com>
2024-12-12 13:41:08 +0100

    Working with all emulator

--- service/service.js
+++ service/service.js
@@ -1,6 +1,6 @@
-var packageId  = tizen.application.getCurrentApplication().appInfo.packageId;
-var applicationId = packageId +'.Jellyfin'
-var remoteMessagePort  = undefined;
+var packageId = tizen.application.getCurrentApplication().appInfo.packageId;
+var applicationId = packageId + '.Jellyfin'
+var remoteMessagePort = undefined;
 var localMessagePort = undefined;
 var watchId = undefined;
 var isServiceStarted = false;
@@ -11,6 +11,7 @@ var isServiceStarted = false;
  * @param {Array} data - Array of key-value pairs received via the message port.
  * @param {Object} remoteMsgPort - The remote message port instance that sent the message.
  */
+
 function onReceived(data, remoteMsgPort) {
 
     sendMessage("Key0 " + data[0].key)
@@ -42,7 +43,8 @@ function onReceived(data, remoteMsgPort) {
 
 
     }
-};
+}; 
+
 
 /**
  * Sends a message to the remote message port.
@@ -53,7 +55,7 @@ function onReceived(data, remoteMsgPort) {
 function sendMessage(value, key) {
     key = key || "KEY";
     if (remoteMessagePort  === undefined) {
-        remoteMessagePort  = tizen.messageport.requestRemoteMessagePort(applicationId, "DATACHANNEL");
+        remoteMessagePort  = tizen.messageport.requestRemoteMessagePort(applicationId, packageId);
     }
     if (remoteMessagePort ) {
         try {
@@ -73,18 +75,20 @@ function sendMessage(value, key) {
 function start() {
     if (isServiceStarted) {
         console.log("Service already started.");
+        sendMessage("Service already started.")
         return;
     }
         try {
             sendMessage("Service Started:" + packageId )
 
-            localMessagePort  = tizen.messageport.requestLocalMessagePort("DATACHANNEL");
+            localMessagePort  = tizen.messageport.requestLocalMessagePort(packageId);
             watchId = localMessagePort.addMessagePortListener(onReceived);
 
             /* For debugging purposes, somehow it is not working on the emulator. */
             sendMessage("WatchID :" + watchId)
             isServiceStarted = true;
             console.log("Service started successfully.");
+            sendMessage("Service started successfully.");
 
         }
         catch (e) {
@@ -94,13 +98,58 @@ function start() {
     
 }
 
+
+function handleDataInRequest()
+{
+    try {
+       
+        var reqAppControl = tizen.application.getCurrentApplication().getRequestedAppControl();
+        if (!!reqAppControl) {
+
+            if (reqAppControl.appControl.data[0].key == 'Preview') {
+                var previewData = reqAppControl.appControl.data[0].value;
+                var previewData2 = JSON.parse(previewData);
+                sendMessage("Preview Data recieved:" + previewData)
+
+                try {
+                    webapis.preview.setPreviewData(JSON.stringify(previewData2),
+                        function () {
+                            console.log('setPreviewData SuccessCallback');
+                            // please terminate service after setting preview data
+                            sendMessage("Preview Set!")
+                            tizen.application.getCurrentApplication().exit();
+                        },
+                        function (e) {
+                            console.log('PreviewData Setting failed : ' + e.message);
+                            sendMessage('PreviewData Setting failed : ' + e.message)
+                        }
+                    );
+                }
+                catch (e) {
+                    sendMessage('PreviewData Setting exception : ' + e.message)
+                }
+            }
+   
+        }
+        else
+        {
+            sendMessage('Unknown Request ')
+        }  
+    }
+    catch (e) {
+        sendMessage('On error exception : ' + e.message)
+    }
+}
+
 module.exports.onStart = function () {
     start();
+    sendMessage("OnStart recieved")
 };
 
-
 module.exports.onRequest = function () {
-    start();
+        start();
+        sendMessage("onRequest recieved");
+        handleDataInRequest();
 }
 
 
